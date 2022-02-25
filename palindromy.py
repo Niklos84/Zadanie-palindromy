6aae5a12fb24246c0341060ca7263a6b55676898
Mikolaj Strzelbicki <mikolaj.strzelbicki@gmail.com>
2022-02-25 21:10:27 +0100

    Rewrite to shorter form

--- palindromy.py
+++ palindromy.py
@@ -2,12 +2,6 @@ def is_palindrom(word):
     """
     Funkcja sprawdza czy podane słowo jest palindromem i zwraca wartość boolean.
     """
-    letters = []
-    for letter in word:
-        letters += letter.lower()
-    rev_letters = []
-    for letter in reversed(word):
-        rev_letters += letter.lower()
-    return letters == rev_letters
+    return word.lower()==word[::-1].lower()
 
 print(is_palindrom('Kajak'))
