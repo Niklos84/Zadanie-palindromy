ca551b36f125bf779631b127e92d0e3f1ce69ca7
Mikolaj Strzelbicki <mikolaj.strzelbicki@gmail.com>
2022-02-25 21:25:16 +0100

    Change doubled functions

--- palindromy.py
+++ palindromy.py
@@ -2,6 +2,6 @@ def is_palindrom(word):
     """
     Funkcja sprawdza czy podane słowo jest palindromem i zwraca wartość boolean.
     """
-    return word.lower()==word[::-1].lower()
-
+    word = word.lower()
+    return word == word[::-1]
 print(is_palindrom('Kajak'))
